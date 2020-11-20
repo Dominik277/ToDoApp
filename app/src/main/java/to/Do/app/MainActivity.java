d80502e2fdc64006d381ec04483dbe03b8688395
Dominik277 <dominikdoric277@gmail.com>
2020-11-20 23:16:06 +0100

    Zapocet Alert Dialog.

--- app/src/main/java/to/Do/app/MainActivity.java
+++ app/src/main/java/to/Do/app/MainActivity.java
@@ -1,32 +1,43 @@
 package to.Do.app;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    View actionItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        actionItem = findViewById(R.id.actionIcon);
 
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.actionIcon:
-                return true;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.actionIcon:
+
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
-
         }
     }
 }
