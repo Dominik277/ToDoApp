64a1acddea78e9a482d197424ebc439c706a5c77
Dominik277 <dominikdoric277@gmail.com>
2020-11-20 21:23:30 +0100

    Dodavanje gumba u Action Bar i dodavanje funkcionalnosti.

--- app/src/main/java/to/Do/app/MainActivity.java
+++ app/src/main/java/to/Do/app/MainActivity.java
@@ -1,14 +1,32 @@
 package to.Do.app;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionIcon:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
