53dafa90a10b4fc80531a927e51a89fc8fac2e8f
Dominik277 <dominikdoric277@gmail.com>
2020-11-21 23:12:46 +0100

    Dodane kontrole za gumbove u Alert Dialogu.

--- app/src/main/java/to/Do/app/MyCustomDialog.java
+++ app/src/main/java/to/Do/app/MyCustomDialog.java
@@ -1,17 +1,20 @@
 package to.Do.app;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class MyCustomDialog extends DialogFragment {
+
+    private static final String TAG = "MyCustomDialog";
 
     public interface OnInputListener{
         void sendInput(String input);
@@ -30,6 +33,32 @@ public class MyCustomDialog extends DialogFragment {
         mInput = view.findViewById(R.id.input);
         mActionOk = view.findViewById(R.id.actionOk);
         mActionCanlcel = view.findViewById(R.id.actionCancel);
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        mActionCanlcel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+
+        mActionOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = mInput.getText().toString();
+                mOnInputListener.sendInput(input);
+                getDialog().dismiss();
+            }
+        });
+                return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            mOnInputListener = (OnInputListener) getActivity();
+        }catch (Exception e){
+            Log.e(TAG,"onAttach: ClassCastException: " + e.getMessage());
+        }
     }
 }
