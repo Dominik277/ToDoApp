3df18773f574b37bcdb719650124590a9dfba128
Dominik277 <dominikdoric277@gmail.com>
2020-11-22 20:20:04 +0100

    Dodan gumb u uglu.

--- app/src/main/java/to/Do/app/MainActivity.java
+++ app/src/main/java/to/Do/app/MainActivity.java
@@ -1,8 +1,12 @@
 package to.Do.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements MyCustomDialog.On
     private Button mOpenDialog;
     private TextView mInputDisplay;
     private String mInput;
+    private View actionItem;
 
 
     @Override
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements MyCustomDialog.On
 
         mOpenDialog = findViewById(R.id.openDialog);
         mInputDisplay = findViewById(R.id.inputDialog);
+        actionItem = findViewById(R.id.actionIcon);
 
         mOpenDialog.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +42,13 @@ public class MainActivity extends AppCompatActivity implements MyCustomDialog.On
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
     public void sendInput(String input) {
 
         Log.d(TAG,"sendInput: got the input: " + input);
@@ -43,8 +56,24 @@ public class MainActivity extends AppCompatActivity implements MyCustomDialog.On
         setInputToTextView();
     }
 
-    private void setInputToTextView(){
+    private void setInputToTextView() {
         mInputDisplay.setText(mInput);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.actionIcon:
+                otvoriAlertDialog();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void otvoriAlertDialog() {
+
+
+
+    }
 }
