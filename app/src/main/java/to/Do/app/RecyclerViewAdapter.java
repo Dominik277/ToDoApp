2ab9cd417f1b31208bc019f80594be02c3ee2bdc
Dominik277 <dominikdoric277@gmail.com>
2020-11-23 11:49:57 +0100

    Adapter za Recycler View gotov.

--- app/src/main/java/to/Do/app/RecyclerViewAdapter.java
+++ app/src/main/java/to/Do/app/RecyclerViewAdapter.java
@@ -1,23 +1,38 @@
 package to.Do.app;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyHolder> {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private LayoutInflater layoutInflater;
+    private List<String> data;
+
+    RecyclerViewAdapter(Context context, List<String> data){
+        this.layoutInflater = LayoutInflater.from(context);
+        this.data = data;
+    }
 
     @NonNull
     @Override
-    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.custom_recyclerview,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        String title = data.get(position);
+        holder.textTitle.setText(title);
 
     }
 
@@ -26,10 +41,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return 0;
     }
 
-    public class MyHolder extends RecyclerView.ViewHolder{
-       public MyHolder(View itemView){
-           super(itemView);
-       }
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView textTitle,textDescription;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.naslovPodsjetnika);
+            textDescription = itemView.findViewById(R.id.opisPodsjetnika);
+        }
     }
 
 }
