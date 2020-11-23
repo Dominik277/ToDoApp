3f1149940b7f885dd575d74f4404524318b2f34e
Dominik277 <dominikdoric277@gmail.com>
2020-11-23 15:52:16 +0100

    Dodan vizualni dio za Main activity_main.xml.

--- app/src/main/java/to/Do/app/MainActivity.java
+++ app/src/main/java/to/Do/app/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -14,58 +16,18 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity implements MyCustomDialog.OnInputListener, MyRecyclerViewAdapter.ItemClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private String mInput;
-    private FloatingActionButton floatingActionButton;
-
+    private TaskDatabaseHelper taskHelper;
+    private ListView TaskList;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        floatingActionButton = findViewById(R.id.floatinActionButton);
-
-        ArrayList<String> items = new ArrayList<>();
-
-        items.add("First CardView Item");
-        items.add("Second CardView Item");
-        items.add("Third CardView Item");
-        items.add("Fourth CardView Item");
-        items.add("Fifth CardView Item");
-        items.add("Sixth CardView Item");
-
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        MyRecyclerViewAdapter recyclerViewAdapter = new MyRecyclerViewAdapter(this, items);
-        recyclerViewAdapter.setClickListener((MyRecyclerViewAdapter.ItemClickListener) this);
-        recyclerView.setAdapter(recyclerViewAdapter);
-
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MyCustomDialog dialog = new MyCustomDialog();
-                dialog.show(getSupportFragmentManager(), null);
-            }
-        });
-
-    }
-
-    @Override
-    public void sendInput(String input) {
-        mInput = input;
-        Log.d(TAG, "sendInput: got the input: " + input);
-
-    }
-
-
-    @Override
-    public void onItemClick(View view, int position) {
-        Toast.makeText(this, "You clicked ", Toast.LENGTH_SHORT)
-                .show();
+        taskHelper = new TaskDatabaseHelper(this);
     }
 }
