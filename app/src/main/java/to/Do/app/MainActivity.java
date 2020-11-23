8741b488f490d68aaacd2ce4f4987072888e6786
Dominik277 <dominikdoric277@gmail.com>
2020-11-23 16:13:09 +0100

    Zavrsena onOptionsItemSelected metoda.

--- app/src/main/java/to/Do/app/MainActivity.java
+++ app/src/main/java/to/Do/app/MainActivity.java
@@ -1,13 +1,20 @@
 package to.Do.app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -29,5 +36,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         taskHelper = new TaskDatabaseHelper(this);
+        TaskList = findViewById(R.id.list_todo);
+
+        updateUI();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_add_task:
+                final EditText taskEdit = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEdit)
+                        .setPositiveButton("Add",(dialog, which)->{
+                            String task = String.valueOf(taskEdit.getText());
+                            SQLiteDatabase db = taskHelper.getWritableDatabase();
+                            ContentValues values = new ContentValues();
+                            values.put(TaskContract.TaskEntry.COL_TASK_TITLE,task);
+                            db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,null,values,SQLiteDatabase.CONFLICT_REPLACE);
+                            db.close();
+                            updateUI();
+            })
+                        .setNegativeButton("Cancel",null).create();
+                dialog.show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
